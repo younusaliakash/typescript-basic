8ea30faa5c3609a8d841ecb683dc48ca205db490
Younus Ali Akash <younusaliakash8@gmail.com>
2021-10-31 20:39:35 +0600

    explicit and union type

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,32 @@
-const test = "This is test Console log";
+// Explicite Types & Union Types
 
-console.log(test);
+let myName: string = "Younus Ali Aksh";
+myName = "Younus Ali";
+
+let myNumber: number = 123456789;
+myNumber = 987456123;
+
+let myArrayOne: string[] = ["Akash", "batas", "Nodi"];
+let myArrayTw0: number[] = [1, 2, 3];
+let myArrayThree: boolean[] = [true, false];
+let myArrayfour: (string | number)[] = ["akash", 5];
+
+function mutiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(mutiply(2, 4));
+
+let myObject: object = {
+  item: "1",
+};
+
+let myObejctTwo: {
+  name: string;
+  phone: number;
+};
+
+myObejctTwo = {
+  name: "Akash",
+  phone: 123456789,
+};
