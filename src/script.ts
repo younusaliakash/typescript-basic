8dac872b2abd25cdae4b856938f17d4a770a8185
Younus Ali Akash <younusaliakash8@gmail.com>
2021-11-01 15:48:04 +0600

    interface

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,15 @@
-class Player {
-  name: string;
-  age: number;
-  country: string;
-  constructor(n: string, a: number, c: string) {
-    this.name = n;
-    this.age = a;
-    this.country = c;
-  }
-  play() {
-    console.log(`${this.name} is played from ${this.country}`);
-  }
+interface SqureOption {
+  height: number;
+  width: number;
 }
 
-const mas = new Player("Mas", 40, "Bangladesh");
+const createSqure = (option: SqureOption) => {
+  console.log(
+    `The Squre Height is ${option.height} and width is ${option.width}`
+  );
+};
+
+createSqure({
+  height: 20,
+  width: 20,
+});
